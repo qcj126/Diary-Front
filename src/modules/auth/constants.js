d50fbf440b07cb4f54a1964bddfe1326fd939a50
qcj126 <1262901257@qq.com>
2026-09-05 00:22:24 +0800

    main

--- src/modules/auth/constants.js
+++
@@ -1,5 +0,0 @@
-import { API_BASE, AUTH_API } from '../../api/index.js'
-
-export { API_BASE }
-
-export const API = AUTH_API
--- src/modules/love-dashboard/constants/api.js
+++
@@ -1,5 +0,0 @@
-import { API_BASE, LOVE_DASHBOARD_API } from '../../../api/index.js'
-
-export { API_BASE }
-
-export const API = LOVE_DASHBOARD_API
